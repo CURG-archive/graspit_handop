9cafc4586d14999b4de8b8f7b2cf108b9b095d7d
Jonathan <jweisz@tonga.(none)>
2013-08-15 12:03:03 -0400

    Changed order of things so that previous jobs are killed before starting a new experiment

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -8,11 +8,13 @@ import ate
 
 def start_experiment(num_ga_iters = 2, num_atr_iters = 2, trials_per_task = 5): 
     interface = eigenhand_db_interface.EGHandDBaseInterface()
-    interface.reset_database()
+    
     task_prototype = eigenhand_db_objects.Task(task_type_id = 4, task_time = 60)
     em = experiment_manager.ExperimentManager(num_ga_iters, num_atr_iters, task_models.full_model_dict,
                                          task_prototype, trials_per_task, ate.weighted_ATE_hand, interface)
-      
+    em.kill_existing()
+    interface.prepare_gen_0()
+    
     em.run_experiment() 
 
 
