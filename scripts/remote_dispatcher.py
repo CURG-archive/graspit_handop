9a19c30915e331c0b3ea01162472202478e77d60
Jonathan <jweisz@tonga.(none)>
2013-08-23 14:44:54 -0400

    many changes to make server launching asynchronous and faster

--- scripts/remote_dispatcher.py
+++ scripts/remote_dispatcher.py
@@ -3,22 +3,22 @@ import sh
 import select
 import time
 import pdb
-
+import threading
+from ctypes import *
 
 class RemoteServer(object):
-    def __init__(self, server_name, interface, launch_job = True):
+    def __init__(self, server_name, interface):
         self.server_name = server_name
-        self.kill_previous()
         self.filename = open("/dev/null","rw")
-        if launch_job:
-            self.subprocess = self.launch_job()
+        self.subprocesses = []
         self.interface = interface
         self.killed_forcibly = False
+        self.kill_previous()
 
     def launch_job(self):
-        args = ["ssh", self.server_name, "python", "/home/jweisz/gm/graspit_dispatcher.py"]
+        args = ["ssh", "-o","ConnectTimeout=30", self.server_name, "python", "/home/jweisz/gm/graspit_dispatcher.py"]
         print "%s \n"%(self.server_name)
-        return subprocess.Popen(args, stdin = subprocess.PIPE, stdout=self.filename, stderr=subprocess.STDOUT)
+        self.subprocesses.append(subprocess.Popen(args, stdin = subprocess.PIPE, stdout=self.filename, stderr=subprocess.STDOUT))
 
     def kill_if_busy(self):       
         args = ["ssh", self.server_name, "python", "/home/jweisz/gm/graspit_dispatch_monitor.py"]
@@ -26,25 +26,29 @@ class RemoteServer(object):
         return subprocess.Popen(args, stdin = subprocess.PIPE, stdout=self.filename, stderr=subprocess.STDOUT)
 
     def kill_previous(self):
-        args = ["ssh", self.server_name, "killall", "python"]
-        s = subprocess.Popen(args, stdin = subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        s.communicate()
-        args = ["ssh", self.server_name, "killall", "graspit"]
-        s = subprocess.Popen(args, stdin = subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        s.communicate()
+        args = ["ssh", "-o","ConnectTimeout=30",self.server_name, "killall", "python"]
+        self.subprocesses.append(subprocess.Popen(args, stdin = subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT))
+        
+        args = ["ssh", "-o","ConnectTimeout=30", self.server_name, "killall", "graspit"]
+        self.subprocesses.append(subprocess.Popen(args, stdin = subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT))
 
+
+    def collect_subprocesses(self):
+        for s in self.subprocesses:
+            s.communicate()
+        self.subprocesses = []
         
     def is_running(self):
-        #self.process_output()
-        return self.subprocess.poll() == None
+        #self.process_output()        
+        return self.subprocesses[0].poll() == None
 
 
     def mark_failed_task(self):
         t = ''
         try:
-            if not select.select([self.subprocess.stdout],[],[],0)[0]:
+            if not select.select([self.subprocesses[0].stdout],[],[],0)[0]:
                 return t
-            t = self.subprocess.stdout.readline()            
+            t = self.subprocesses[0].stdout.readline()            
             line_list = t.split(' ')            
             job_id_index = line_list.index('unfinished') - 1
             job_id = int(line_list[job_id_index])
@@ -54,42 +58,110 @@ class RemoteServer(object):
             pass
         return t
 
+    def kill_all_subprocesses(self):
+        for s in self.subprocesses:
+            try:
+                s.kill()
+            except:
+                pass
+            
     def process_output(self):
         while self.mark_failed_task() != '':
             continue
                 
-            
+           
 
-
+import pdb
 class RemoteDispatcher(object):
     def __init__(self, server_name_dict, interface):
         self.server_dict = dict()
+        self.server_starting_dict = dict()
         self.interface = interface
+        self.thread_list = []
+        print "server name dict"
+        print server_name_dict.keys()
         for server in server_name_dict:
-            try:
-                a = sh.ping(server,w=1)
-            except:
-                print "Couldn't ping %s"%(server)
-                continue
-            if a.exit_code:
-                print "Couldn't ping %s"%(server)
-                continue
-            print "pinged %s"%(server)                
-            self.server_dict[server] = RemoteServer(server, interface)
+            self.thread_list.append(threading.Thread(target = self.init_server, args=(server,)))
+            self.thread_list[-1].start()
+        print "waiting for threads to join"
+        #for thread in thread_list:
+        #    thread.join()
+        print "Threads joined"
+        
+        self.file = open('/var/www/eigenhand_project/running_jobs.shtml','w')
 
-        self.file = open('/tmp/running_jobs.txt','w')
+    def init_server(self, server_name):
+        """
+        try:
+            a = sh.ping(server_name,w=1)
+        except:
+            print "Couldn't ping %s"%(server_name)
+            return
+        if a.exit_code:
+            print "Couldn't ping %s"%(server_name)
+            return
+        print "pinged %s"%(server_name)
+        """
+        try:
+            a = RemoteServer(server_name, self.interface)
+            self.server_starting_dict[server_name] = a
+            a.kill_previous()
+            self.start_server(a)
+            self.server_dict[server_name] = a
+        except:
+            print "server :%s failed to start"%(server_name)
+        
 
-    def run(self):
-        running = 1
+    def start_server(self, server):
+        server.collect_subprocesses()
+        server.launch_job()
             
-        while running:            
-            running = len([server for server in self.server_dict.values() if server.is_running()])
+    def run(self, max_len = 1200):
+        running = 1
+        t = time.time()
+        generation = self.interface.get_max_hand_gen()
+        self.file.write('started running generation %i at %s \n'%(generation,time.strftime('%c')))
+        self.file.seek(0)
+        self.file.flush()
+        time.sleep(5)
+        running_servers = []
+        while running and self.interface.get_num_incompletes() > 0 and time.time() - t < max_len:
+            test_dict = dict(self.server_dict)
+            running_servers = [server for server in test_dict.values() if server.is_running()]
+
+            if self.server_dict.values() != []:
+                running = len(running_servers)
             self.file.seek(0)
-            self.file.write('Running processes %i %s \n'%(running, time.strftime('%c')))
+            self.file.write('Generation %i, Running processes %i jobs %i %s \n'%(generation, running, self.interface.get_num_incompletes(), time.strftime('%c')))
+            self.file.write(' '.join([server.server_name for server in running_servers]) + '\n')
+            self.file.truncate()
             self.file.flush()
             time.sleep(1)
+        finished_string = 'Finished running generation %i. time taken %i num incompletes %i. time %s \n'%(self.interface.get_max_hand_gen(), time.time() - t,
+                                                                                                 self.interface.get_num_incompletes(),
+                                                                                                time.strftime('%c'))
+        print finished_string
+        if running_servers:
+            print running_servers
+        
+        self.file.seek(0)
+        self.file.write(finished_string)
+        self.file.truncate()
+        self.file.flush()
+        self.file.close()
+        for server in self.server_starting_dict.values():
+            try:
+                server.kill_all_subprocesses()
+            except:
+                pass
             
-    
+            server.filename.close()
+        for thread in self.thread_list:
+            if thread.is_alive():
+                pthread = cdll.LoadLibrary("libpthread-2.10.1.so")
+                pthread.pthread_cancel(c_ulong(thread.ident))
+                
+        
     def run_monitored(self, monitor_functor = []):
         running = 1
         while running:
