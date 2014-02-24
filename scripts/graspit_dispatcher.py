e9c53598ba94b28eeb69e2d77f6c967249e5b347
Ben Caimano <bcaimano@gmail.com>
2014-02-24 13:55:24 -0500

    They're sorta thorough

--- scripts/graspit_dispatcher.py
+++ scripts/graspit_dispatcher.py
@@ -164,8 +164,8 @@ class LocalDispatcher(object):
 
         self.get_idle_percent()
 
-        self.cursor.execute("DELETE FROM servers WHERE server_name = '%s';"%(self.server_name))
-        self.cursor.execute("INSERT INTO servers (server_name,ip_addr,idle_percent,num_processors,running_jobs,paused_jobs) VALUES ('%s','%s','%s','%s','%s','%s');"%(self.server_name,self.ip_addr,self.idle_percent,self.num_processors,num_running,num_paused))
+        self.cursor.execute("DELETE FROM servers WHERE server_name = %s;",(self.server_name))
+        self.cursor.execute("INSERT INTO servers (server_name,ip_addr,idle_percent,num_processors,running_jobs,paused_jobs) VALUES (%s,%s,%s,%s,%s,%s);",(self.server_name,self.ip_addr,self.idle_percent,self.num_processors,num_running,num_paused))
         self.connection.commit()        
 
 	self.output_status(num_running)
@@ -217,13 +217,13 @@ class LocalJob(object):
         self.log("Starting process from graspit_dispatcher")
         self.subprocess = subprocess.Popen(args, stdin = subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        self.dispatcher.cursor.execute("INSERT INTO jobs (server_name, job_lid, last_updated) VALUES('%s',%s,now())",[self.dispatcher.server_name,self.job_lid])
+        self.dispatcher.cursor.execute("INSERT INTO jobs (server_name, job_lid, last_updated) VALUES(%s,%s,now())",[self.dispatcher.server_name,self.job_lid])
         self.connection.commit()        
 
     def log(self,message):
         timestamp = datetime.datetime.now().isoformat()
         self.log_file.write("%s local job %s: %s\n"%(timestamp,self.job_lid,message))
-        self.dispatcher.cursor.execute("INSERT INTO log(server_name, message) VALUES('%s','task %s: %s')",[self.dispatcher.server_name,self.job_lid,message])
+        self.dispatcher.cursor.execute("INSERT INTO log(server_name, message) VALUES(%s,%s)",[self.dispatcher.server_name,'task %i: %s'%(self.job_lid,message)])
         self.connection.commit()        
 
     def poll(self):
@@ -247,10 +247,10 @@ class LocalJob(object):
             self.dispatcher.can_launch = (self.exit_code != 5) #I/O IS IMPORTANT
 
             self.log("Process finished with return code %i"%self.exit_code)
-            self.dispatcher.cursor.execute("UPDATE jobs (exit_code,end_time,last_updated) VALUES (%s,now(),now()) WHERE server_name = '%s', job_lid = %s;",[self.exit_code,self.dispatcher.server_name,self.job_lid])
+            self.dispatcher.cursor.execute("UPDATE jobs (exit_code,end_time,last_updated) VALUES (%s,now(),now()) WHERE server_name = %s, job_lid = %s;",[self.exit_code,self.dispatcher.server_name,self.job_lid])
             self.connection.commit()        
         else:
-            self.dispatcher.cursor.execute("UPDATE jobs (last_updated) VALUES (now()) WHERE server_name = '%s', job_lid = %s;",[self.dispatcher.server_name,self.job_lid])
+            self.dispatcher.cursor.execute("UPDATE jobs (last_updated) VALUES (now()) WHERE server_name = %s, job_lid = %s;",[self.dispatcher.server_name,self.job_lid])
             self.connection.commit()        
 
     def is_running(self):
