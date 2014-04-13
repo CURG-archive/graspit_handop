5e1aeaf9bde804f77a7e00a2a992b8876299a7ac
Ben Caimano <bcaimano@gmail.com>
2014-04-13 17:42:07 -0400

    Renaming some things and separate a function

--- scripts/experiment_manager.py
+++ scripts/experiment_manager.py
@@ -2,7 +2,7 @@ import time
 import os
 
 import remote_dispatcher,  generation_manager
-import eigenhand_db_interface, eigenhand_db_tools, eigenhand_db_objects
+import eigenhand_interface, eigenhand_db_tools, eigenhand_db_objects
 import atr, ate
 import eigenhand_genetic_algorithm
 import task_models, server_list
@@ -13,7 +13,7 @@ class ExperimentManager(object):
                  eval_functor = ate.weighted_threshold_ATE_hand,
                  server_dict = server_list.clic_lab_dict,
                  task_prototype = eigenhand_db_objects.Task(task_type_id = 4, task_time = -1),
-                 db_interface = eigenhand_db_interface.EGHandDBaseInterface()):
+                 interface = eigenhand_db_interface.EGHandDBaseInterface()):
         """
         @param num_ga_iters - The number of genetic algorithm generations to run
         @param num_atr_iters - The number of ATR iterations to run for each GA generation
@@ -21,7 +21,7 @@ class ExperimentManager(object):
         @param task_prototype - A prototype Task object that defines the trial specification(i.e. length and type)
         @param trials_per_task - The number of planning jobs to run for each object/hand pair per planning iteration. Results from each should look fairly unique.
         @param eval_functor - The function to use when evaluating hand fitness in the genetic algorithm
-        @param db_interface - The database interface to use. Assumed to be initialized with a starting set of 0th generation hands
+        @param interface - The database interface to use. Assumed to be initialized with a starting set of 0th generation hands
         @param starting_ga_iter - The generation to start from.
         @param server_dict - A dictionary whose keys are server urls that will be filled with
                              Servers as connections are made
@@ -29,7 +29,7 @@ class ExperimentManager(object):
         self.task_prototype = task_prototype
         self.task_model_list = task_model_list
         self.eval_functor = eval_functor
-        self.db_interface = db_interface
+        self.interface = interface
         self.config = config
         self.server_dict = server_dict
         self.e_list = dict()
@@ -37,14 +37,14 @@ class ExperimentManager(object):
         self.gm = []
         self.rd = []       #self.score_array = numpy.array([4,0])
         
-        self.db_interface.update_config(config) 
+        self.interface.update_config(config) 
 
     def initialize_generation_manager(self):
         """
         @brief Create the generation manager that will be used in this experiment
         """
-        return generation_manager.GenerationManager(self.db_interface, self.task_model_list,
-                                                    self.db_interface.get_max_hand_gen(),
+        return generation_manager.GenerationManager(self.interface, self.task_model_list,
+                                                    self.interface.get_max_hand_gen(),
                                                     self.config['task_time'], self.config['trials_per_task'],
                                                     self.config['task_type_id'],
                                                     self.eval_functor)
@@ -66,7 +66,7 @@ class ExperimentManager(object):
         t = time.time()
         r = 0
         #Test if there are jobs available to do.
-        job_num = eigenhand_db_tools.get_num_unlaunched_jobs(self.db_interface)
+        job_num = eigenhand_db_tools.get_num_unlaunched_jobs(self.interface)
 
         #Try to run a new remote dispatcher loop as long as there are unfinished jobs
         while job_num > 2:
@@ -76,47 +76,52 @@ class ExperimentManager(object):
             self.rd.run()
             #Reset any incomplete jobs. These are essentially jobs that we have lost connection with and cannot be
             #relied upon to terminate.
-            self.db_interface.reset_incompletes()
+            self.interface.reset_incompletes()
             #See how many jobs are still undone.
-            job_num = eigenhand_db_tools.get_num_unlaunched_jobs(self.db_interface)
+            job_num = eigenhand_db_tools.get_num_unlaunched_jobs(self.interface)
         #If we only have a few stragglers, just stop trying -- We don't really need all of them to exit cleanly,
         #and it is easier to fail gracefully than try to handle every error.
-        self.db_interface.set_incompletes_as_error()
+        self.interface.set_incompletes_as_error()
         print "done.  Time %f \n"%(time.time() - t)
 
     def backup_results(self):
-        self.db_interface.incremental_backup(experiment_name=self.config['name'],generation=self.gm.generation)
+        self.interface.incremental_backup(experiment_name=self.config['name'],generation=self.gm.generation)
 
     def restore_results(self, generation=0):
-        self.db_interface.incremental_restore(experiment_name=self.config['name'],generation=self.gm.generation)
+        self.interface.incremental_restore(experiment_name=self.config['name'],generation=self.gm.generation)
     
     def restore_all(self):
-        self.db_interface.reset_database()
+        self.interface.reset_database()
         self.restore_hands()
         self.restore_all_grasps()
 
     def restore_to_new_dbase(self):
-        self.db_interface.prepare_empty_db(self.experiment_name)
-        self.db_interface = eigenhand_db_interface.EGHandDBaseInterface(self.experiment_name)
+        self.interface.prepare_empty_db(self.experiment_name)
+        self.interface = eigenhand_db_interface.EGHandDBaseInterface(self.experiment_name)
         self.restore_all()
 
     def drop_dbase(self):
-        self.db_interface.drop_database(self.experiment_name)
+        self.interface.drop_database(self.experiment_name)
+
+    def prepare_experiment(self):
+        self.interface.reset_database()
+        self.interface.prepare_gen_0()
+
+        #initialize new generation manager to configure the database to start running.
+        self.gm = self.initialize_generation_manager()
+        self.gm.next_generation(gen_type=0)
+
+        #Build the new remote dispatcher and connect all the servers
+        self.rd = remote_dispatcher.RemoteDispatcher(self.interface)
+        self.rd.init_all_servers(self.server_dict)
+        self.run_remote_dispatcher_tasks()
+        
 
     def run_experiment(self):
         """
         @brief Run the whole experiment. Does num_ga_iters genetic algorithm runs each containing num_atr iterations
         of ATR.
         """
-        #initialize new generation manager to configure the database to start running.
-        self.gm = self.initialize_generation_manager()
-        self.gm.next_generation(gen_type=0)
-
-        #Build the new remote dispatcher and connect all the servers
-        self.rd = remote_dispatcher.RemoteDispatcher(self.db_interface)
-        self.rd.init_all_servers(self.server_dict)
-        self.run_remote_dispatcher_tasks()
-
         #Run through a bunch of iterations
         generations = ([1]*self.config['atr_iterations'] + [2])*self.config['ga_iterations']
 
@@ -135,7 +140,7 @@ class ExperimentManager(object):
                 new_hand_list = eigenhand_genetic_algorithm.GA_generation(grasp_list, self.gm.hands, self.eval_functor, .5-.4/(self.config['ga_iterations']*generation/(1+self.config['atr_iterations'])))
 
             #Put the new hands in to the database.
-            eigenhand_db_tools.insert_unique_hand_list(new_hand_list, self.db_interface)
+            eigenhand_db_tools.insert_unique_hand_list(new_hand_list, self.interface)
 
             #Backup results and then remove everything from the grasp table
             self.backup_results
@@ -165,8 +170,8 @@ def new_em(name, ga_iterations, atr_iterations = 5, task_model_names = task_mode
     return em
 
 def resume_em():
-    db_interface = eigenhand_db_interface.EGHandDBaseInterface()
-    config = db_interface.load_config()
+    interface = eigenhand_db_interface.EGHandDBaseInterface()
+    config = interface.load_config()
     task_model_list = task_models.model_set(config['task_models'])
-    em = ExperimentManager(config,task_model_list)
+    em = ExperimentManager(config,task_model_list,interface=interface)
     return em
